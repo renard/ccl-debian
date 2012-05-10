41f2ef4c9dbd7b37de8a8ac4b1a608ec2a23991f
Sébastien Gross <sebchezwamorg>
2012-05-10 15:45:13 +0200

    Factorize build-ccl code.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- build-ccl.lisp
+++ build-ccl.lisp
@@ -13,11 +13,9 @@
 ;; See http://permalink.gmane.org/gmane.lisp.openmcl.devel/7530
 (defparameter ccl::fasl-version (ccl::target-fasl-version))
 
-(common-lisp-controller:compile-common-lisp-controller-v5 "ccl")
-(load "/var/cache/common-lisp-controller/0/ccl/common-lisp-controller/common-lisp-controller.lx64fsl")
-(load "/var/cache/common-lisp-controller/0/ccl/cl-asdf/asdf.lx64fsl")
-(load "/var/cache/common-lisp-controller/0/ccl/cl-asdf/wild-modules.lx64fsl")
-(load "/var/cache/common-lisp-controller/0/ccl/common-lisp-controller/post-sysdef-install.lx64fsl")
+(mapcar #'load
+ (common-lisp-controller:compile-common-lisp-controller-v5 "ccl"))
+
 (common-lisp-controller:init-common-lisp-controller-v5 "ccl")
 
-(ccl:save-application "ccl-deb")
+(ccl:save-application (if (member :X86 *features*) "ccl-deb" "ccl-deb64"))
